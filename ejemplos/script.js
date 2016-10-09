2f355aa9cebc74bd4bd3eec591e32660f6090a75
Ruben Lopez <rubenlova.95@gmail.com>
2016-10-09 20:40:54 +0200

    dia 09

---
+++ ejemplos/script.js
@@ -0,0 +1,36 @@
+var interval;
+
+function webCargada () {
+	comprobarFecha();
+}
+
+function toggleRefreshButton () {
+
+	var boton = document.getElementById('refreshButton');
+	if (boton.innerHTML == "Parar Refresco")
+	{
+		if (confirm("Seguro que quieres parar el reloj?"))
+		{
+			clearInterval(interval);
+			boton.innerHTML = "Iniciar Refresco";
+		}
+	}
+	else
+	{
+		interval = setInterval(comprobarFecha, 100);
+		boton.innerHTML = "Parar Refresco";
+	}
+}
+
+function comprobarFecha() {
+	var d = new Date();
+	document.getElementById('hora').innerHTML = d;
+}
+
+function timeout() {
+	var tiempoCampo = parseInt(document.getElementById('stopTime').value);
+	clearInterval(interval);
+	setTimeout(function() {
+		interval = setInterval(comprobarFecha, 100)
+	}, tiempoCampo * 1000);
+}
