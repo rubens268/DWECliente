980925d9d5d373eb74406b848595fe7c63a55612
ciclos <ciclos>
2016-10-11 17:01:03 +0200

    repair

--- ejemplos/script.js
+++ ejemplos/script.js
@@ -1,9 +1,17 @@
+// Guarda la id del temporizador
 var interval;
 
+/* Esta function es llamada al cargar la web y sirve para 
+ * rellenar la web nada mas cargarse
+ */ 
 function webCargada () {
 	comprobarFecha();
 }
 
+/*
+ * Esta function es llamada por el boton y sirve para
+ * Iniciar o parar el temporizador
+ */
 function toggleRefreshButton () {
 
 	var boton = document.getElementById('refreshButton');
@@ -22,11 +30,15 @@ function toggleRefreshButton () {
 	}
 }
 
+//Esta function actualiza el elemento con id hora 
 function comprobarFecha() {
 	var d = new Date();
 	document.getElementById('hora').innerHTML = d;
 }
 
+/* Esta function es llamada por el boton de enviar y sirve para 
+ * parar el contador y lo inicia al pasar "x" segundos
+ */
 function timeout() {
 	var tiempoCampo = parseInt(document.getElementById('stopTime').value);
 	if (!isNaN(tiempoCampo) && isFinite(tiempoCampo))
