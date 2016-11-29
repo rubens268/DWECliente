afa6bd57f772d25ab3c2f10c67cc904005260938
Ruben Lopez <rubenlova.95@gmail.com>
2016-11-29 20:19:42 +0100

    gg

---
+++ U5/Ej10/script.js
@@ -0,0 +1,23 @@
+document.querySelector('img').style.width = "150px"; 
+document.querySelector('img').style.height = "150px";
+
+onkeyup = function (e) {
+    var imagen = document.querySelector('img'); 
+    var currentSize = parseInt(imagen.style.width.substring(0, imagen.style.width.length - 2));
+    if ((e.keyCode == 45) ||(e.keyCode == 109))
+    {
+        if (currentSize > 150)
+        {
+            imagen.style.width = currentSize - 25 + "px";
+            imagen.style.height = currentSize - 25 + "px";
+        }
+    }
+    else if ((e.keyCode == 43) ||(e.keyCode == 107))
+    {
+        if (currentSize < 1024)
+        {
+            imagen.style.width = currentSize + 25 + "px";
+            imagen.style.height = currentSize + 25 + "px";
+        }
+    }
+}
---
+++ U5/Ej9/script.js
@@ -0,0 +1,42 @@
+document.getElementById('cuadrado').style.top = "80px"; 
+document.getElementById('cuadrado').style.left = "20px";
+
+onkeyup = function (e) {
+    var cuadrado = document.getElementById('cuadrado'); 
+    // Arriba
+    if (e.keyCode == 38)
+    {
+        if (cuadrado.offsetTop > 0)
+        {
+            var currentPosition = parseInt(cuadrado.style.top.substring(0, cuadrado.style.top.length - 2));
+            cuadrado.style.top = currentPosition - 20 + "px";
+        }
+    }
+    //Abajo
+    else if (e.keyCode == 40)
+    {
+        if (cuadrado.offsetTop < document.body.offsetHeight - 220)
+        {
+            var currentPosition = parseInt(cuadrado.style.top.substring(0, cuadrado.style.top.length - 2));
+            cuadrado.style.top = currentPosition + 20 + "px";
+        }
+    }
+    // Izquierda
+    else if (e.keyCode == 37)
+    {
+        if (cuadrado.offsetLeft > 0)
+        {
+            var currentPosition = parseInt(cuadrado.style.left.substring(0, cuadrado.style.left.length - 2));
+            cuadrado.style.left = currentPosition - 20 + "px";
+        }
+    }
+    // Derecha
+    else if (e.keyCode == 39)
+    {
+        if (cuadrado.offsetLeft < document.body.offsetWidth - 200)
+        {
+            var currentPosition = parseInt(cuadrado.style.left.substring(0, cuadrado.style.left.length - 2));
+            cuadrado.style.left = currentPosition + 20 + "px";
+        }
+    }    
+}
