ef6898979bf4d4c124d52a27390e083402e83077
Ruben Lopez <rubenlova.95@gmail.com>
2016-11-30 21:45:59 +0100

    gg

--- U5/Practica2/script.js
+++ U5/Practica2/script.js
@@ -1,14 +1,14 @@
 var turno = "X";
 var fichasX = 0;
 var fichasO = 0;
-
+var ganado;
 
 function dom () {
     var botones = document.getElementsByTagName('button');
     for (var boton of botones)
     {
         boton.onclick = function () {
-            if (this.innerHTML == "")
+            if ((this.innerHTML == "")&&(!ganado)){
                 if (turno == "X") 
                 {
                     if (fichasX < 3)
@@ -35,11 +35,26 @@ function dom () {
                     {
                         alert("Hay demasiadas fichas O");
                     }
-                } 
-            document.getElementById('turno').innerHTML = turno;
+                }
+                document.getElementById('turno').innerHTML = turno; 
+            }
+                
+            if (!ganado){
+                var winner = comprobarTresEnRaya();
+                if (winner != false) {
+                    alert("Tres en raya, gana " + winner);
+                    var text = document.getElementById('turno').parentNode;
+                    text.innerHTML = "Tres en raya, gana " + winner;
+                    text.style.color = "red";
+                    text.style.fontWeight = "bold";
+                    ganado = true;
+                }
+            }
+                
         }
 
         boton.ondblclick = function () {
+            if (ganado) { return; }
             if (this.innerHTML != turno)
             {
                 alert("No es tu turno");
@@ -63,11 +78,25 @@ function comprobarTresEnRaya () {
     for (var boton of botones)
     {
         if (boton.innerHTML == "X")
-            botonesX.push(boton);
-        if (boton.innerHTML == "Y")
-            botonesY.push(boton);
+            botonesX.push(boton.id);
+        if (boton.innerHTML == "O")
+            botonesY.push(boton.id);
     }
 
-    
-    
+    if (botonesX.length == 3)
+    {
+        botonesX.sort();
+        if ((parseInt(botonesX[0][1])+1 == parseInt(botonesX[1][1]) && parseInt(botonesX[1][1]) == parseInt(botonesX[2][1])-1) ||
+            (parseInt(botonesX[0][0])+1 == parseInt(botonesX[1][0]) && parseInt(botonesX[1][0]) == parseInt(botonesX[2][0])-1))
+            return "X";
+    }
+    else if (botonesY.length == 3)
+    {
+        botonesY.sort();
+        if ((parseInt(botonesY[0][1])+1 == parseInt(botonesY[1][1]) && parseInt(botonesY[1][1]) == parseInt(botonesY[2][1])-1) ||
+            (parseInt(botonesY[0][0])+1 == parseInt(botonesY[1][0]) && parseInt(botonesY[1][0]) == parseInt(botonesY[2][0])-1))
+            return "O";
+    }
+    return false;
 }
+
